c8ee2368d236b968eb0c4593aafa03c70ff4590f
Maxwell <mboakye@excenit.com>
2023-03-06 12:33:55 +0000

    modified cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'], //changed from wildcard * to 'http://localhost:3000
 
     'allowed_origins_patterns' => [],
 
